713cc78a5dacbe0af63c16deb0d358b7495412a1
Jason Pilz <jasonpilz@gmail.com>
2016-03-10 16:25:34 -0700

    add electro + webLed

--- electro+mosfetled.ino
+++ electro+mosfetled.ino
@@ -1,17 +1,22 @@
 int vPin    = A5;
-double vPow = 3.33;
+int ledPin  = D0;
+double vPow = 3.353;
 double r1   = 82000;
 double r2   = 10000;
 double voltageValue = 0;
 double maxInput = 0;
 
 void setup() {
-  pinMode(vPin,INPUT);
+  pinMode(vPin, INPUT);
+  pinMode(ledPin, OUTPUT);
 
   Particle.variable("voltage", voltageValue);
   Particle.variable("maxvoltage", maxInput);
 
+  Particle.function("led", ledToggle);
+
   maxInput = (vPow / (r2 / (r1 + r2)));
+  digitalWrite(ledPin, LOW);
 }
 
 void loop() {
@@ -23,4 +28,25 @@ void loop() {
   voltageValue = v2;
 
   delay(5000);
+}
+
+int ledToggle(String command) {
+
+  // NOTES:
+  // VIN = 4.68 V
+  // Led FV = 1.61 V
+  // Led I  = 0.015 amp
+  // Rdrop = 3.07 V
+
+  if (command=="on") {
+    digitalWrite(ledPin, HIGH);
+    return 1;
+  }
+  else if (command=="off") {
+    digitalWrite(ledPin, LOW);
+    return 0;
+  }
+  else {
+    return -1;
+  }
 }
