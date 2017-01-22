3198dc71d39a68689fa44a20c4b55f451f108e2e
Jason Pilz <jasonpilz@gmail.com>
2017-01-21 23:07:27 -0800

    Revise weathershield dashboard formatting and variable types

--- weathershield/weathershield-dashboard.ino
+++ weathershield/weathershield-dashboard.ino
@@ -2,12 +2,13 @@
 
 Weather sensor;
 
-float tempF = 0;
-float t     = 0;  // actual temperature
-float h     = 0;  // humidity
+double tempF = 0;
+double t     = 0;  // actual temperature
+double h     = 0;  // humidity
 
 void setup() {
   Particle.variable("tempF", tempF);
+  Particle.variable("tempC", t);
   Particle.variable("humidity", h);
 
   sensor.begin();
@@ -24,13 +25,14 @@ void loop() {
 
 void getClimateData() {
   tempF = sensor.getTempF();
-  t = (tempF - 32) * 0.5555555556;
-  h = sensor.getRH();
+  t     = (tempF - 32) * 0.5555555556;
+  h     = sensor.getRH();
 }
 
 void publishData() {
   char payload[255];
 
+  //  { "t": ##.#, "h": ##.# }
   snprintf(payload, sizeof(payload), "{ \"t\": %.3f, \"h\": %.1f }", t, h);
   Particle.publish("climate", payload, 60);
 }
