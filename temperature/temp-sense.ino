e652f841971f18facef141452aa6c6cbc6d5f326
Jason Pilz <jasonpilz@gmail.com>
2017-01-22 16:18:55 -0800

    Remove comments

--- temperature/temp-sense.ino
+++ temperature/temp-sense.ino
@@ -3,14 +3,12 @@
 int tempPin   = A5;
 int analogVal = 0;
 double vPow   = 3.2;
-double tempF  = 0;
 double tempC  = 0;
 
 void setup() {
   pinMode(tempPin, INPUT);
 
   Particle.variable("tempC", tempC);
-  // Particle.variable("tempF", tempF);
 }
 
 void loop() {
@@ -23,15 +21,12 @@ void loop() {
 
 void getTemperature() {
   analogVal = analogRead(tempPin);
-  //tempF     = (analogVal * vPow) / 4095.0;
-  //tempC     = (tempF - 32) * 0.5555555556;
   tempC = (((analogVal * vPow) / 4095) - 0.5) * 100;
 }
 
 void publishData() {
   char payload[255];
 
-  //snprintf(payload, sizeof(payload), "{ \"°C\": %.3f, \"°F\": %1.f }", tempC, tempF);
   snprintf(payload, sizeof(payload), "{ \"C\": %.3f }", tempC);
   Particle.publish("temperature", payload, 60);
 }
